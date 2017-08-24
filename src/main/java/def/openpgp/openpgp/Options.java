d8a0da88eb4d0fcd50b2c9689258ad13b732fb92
Aleksandar Ivanov <smichess@gmail.com>
2017-08-24 14:41:15 +0300

    adding message and privatekey

--- src/main/java/def/openpgp/openpgp/Options.java
+++ src/main/java/def/openpgp/openpgp/Options.java
@@ -3,10 +3,14 @@ import def.js.Array;
 @jsweet.lang.Interface
 public abstract class Options extends def.js.Object {
 	@jsweet.lang.Optional
-    public Object data;
+    public String data;
+	@jsweet.lang.Optional
+    public Object message;
     @jsweet.lang.Optional
     public Array<def.openpgp.openpgp.key.Key> publicKeys;
     @jsweet.lang.Optional
     public Array<def.openpgp.openpgp.key.Key> privateKeys;
+    @jsweet.lang.Optional
+    public def.openpgp.openpgp.key.Key privateKey;
 }
 
